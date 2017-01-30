78fb7b0a8ba196b4d319970f4edb9cbab3754d0c
Eldho Mathulla <eldhomathulla@gmail.com>
2017-01-29 22:47:05 -0500

    #5 Adding service registration to th api server using curator

--- mock-airavata-api-server/src/main/java/org/apache/airavata/api/server/MockAiravataAPIServer.java
+++ mock-airavata-api-server/src/main/java/org/apache/airavata/api/server/MockAiravataAPIServer.java
@@ -21,15 +21,30 @@
 
 package org.apache.airavata.api.server;
 
+import java.nio.ByteBuffer;
+
 import org.apache.airavata.api.credentials.CredentialManagementService;
 import org.apache.airavata.api.gateway.management.GatewayManagementService;
 import org.apache.airavata.api.handlers.CredentialManagementHandler;
 import org.apache.airavata.api.handlers.GatewayManagementHandler;
+import org.apache.curator.generated.CuratorException;
+import org.apache.curator.generated.CuratorProjection;
+import org.apache.curator.generated.CuratorService;
+import org.apache.curator.generated.DiscoveryInstance;
+import org.apache.curator.generated.DiscoveryProjection;
+import org.apache.curator.generated.DiscoveryService;
+import org.apache.curator.generated.DiscoveryServiceLowLevel;
+import org.apache.thrift.TException;
 import org.apache.thrift.TMultiplexedProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,8 +58,15 @@ public class MockAiravataAPIServer {
     public static GatewayManagementHandler gatewayManagementHandler;
     public static GatewayManagementService.Processor gatewayManagementProcessor;
 
+	private static TTransport tTransport;
+
+	private static DiscoveryServiceLowLevel.Client discoveryServiceClient;
+
+	private static DiscoveryInstance discoveryInstance;
+
     public static void main(String [] args) {
         try {
+        	register();
             credentialManagementHandler = new CredentialManagementHandler();
             credentialManagementProcessor = new CredentialManagementService.Processor(credentialManagementHandler);
 
@@ -65,7 +87,27 @@ public class MockAiravataAPIServer {
 
         } catch (Exception x) {
             x.printStackTrace();
-        }
+        }finally {
+			if(tTransport!=null){
+				tTransport.close();
+			}
+		}
+    }
+    
+    
+    private static void register() throws CuratorException, TException{
+    	tTransport = new TSocket("localhost", 9090);
+    	tTransport.open();
+    	TProtocolFactory tProtocolFactory=new TBinaryProtocol.Factory();
+    	TProtocol tProtocol=tProtocolFactory.getProtocol(tTransport);
+    	discoveryServiceClient = new DiscoveryServiceLowLevel.Client(tProtocol);
+    	CuratorProjection curatorProjection=new CuratorService.Client(tProtocol).newCuratorProjection("main");
+    	DiscoveryService.Client discoveryServiceOrClient=new DiscoveryService.Client(tProtocol);
+    	discoveryInstance = discoveryServiceOrClient.makeDiscoveryInstance("MicroService", ByteBuffer.allocate(1), 9190);
+    	discoveryInstance.address="localhost";
+    	DiscoveryProjection discoveryProjection=discoveryServiceOrClient.startDiscovery(curatorProjection, "services", discoveryInstance);
+    	discoveryServiceClient.registerInstance(curatorProjection, discoveryProjection, discoveryInstance);
+    	
     }
 
 
