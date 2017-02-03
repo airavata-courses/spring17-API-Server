278ab4101f30ed23f906c1f66f89228d3f4e20fe
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2017-02-03 15:15:41 -0500

    fix java consul integration

--- build-scripts/codeDeployInstall.sh
+++ build-scripts/codeDeployInstall.sh
@@ -6,6 +6,6 @@ mvn clean install
 nohup java -jar mock-airavata-api-server/target/mock-airavata-api-server-0.15-SNAPSHOT.jar &
 
 # Registering the service to consul using a python script
-cd /home/ec2-user/api-deployment/spring17-API-Server/build-scripts
-sudo pip install python-consul
-python register_service_consul.py
+# cd /home/ec2-user/api-deployment/spring17-API-Server/build-scripts
+# sudo pip install python-consul
+# python register_service_consul.py
