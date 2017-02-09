bc20743af6c6a437f584926541acbbb090c57125
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2017-02-09 01:41:20 -0500

    fix userdata for loadbalancer

--- build-scripts/codeDeployBeforeInstall.sh
+++ build-scripts/codeDeployBeforeInstall.sh
@@ -1,5 +1,1 @@
 #!/usr/bin/env bash
-
-cd /home/ec2-user/api-deployment/
-rm -rf spring17-API-Server
-mkdir spring17-API-Server
--- build-scripts/codeDeployInstall.sh
+++ build-scripts/codeDeployInstall.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-cd /home/ec2-user/api-deployment/spring17-API-Server/
-mvn clean install
-nohup java -jar mock-airavata-api-server/target/mock-airavata-api-server-0.15-SNAPSHOT.jar &
+echo 'send HUP signal to reload consul-template config'
+sudo ps -efww | grep consul-template | awk '{print $2}' | xargs kill -HUP
