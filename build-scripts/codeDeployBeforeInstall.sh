c08310b83d9532db81f443d7cf2fadc85989fc15
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2017-02-10 15:43:33 -0500

    change file url

--- build-scripts/codeDeployBeforeInstall.sh
+++ build-scripts/codeDeployBeforeInstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #overwrite haproxy-config folder with new values
-sudo mv -f haproxy-config/ /home/ec2-user/haproxy-config
+sudo mv -f  /home/ec2-user/codeDeploy/haproxy-config/ /home/ec2-user/haproxy-config
