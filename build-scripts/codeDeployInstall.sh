c3e41e35b6d506e1e2c2e54fc6b028bf7638f2a3
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2017-02-10 15:50:13 -0500

    change kill to use pid file

--- build-scripts/codeDeployInstall.sh
+++ build-scripts/codeDeployInstall.sh
@@ -4,4 +4,4 @@
 sudo mv -f  /home/ec2-user/codeDeploy/haproxy-config/ /home/ec2-user/haproxy-config
 
 echo 'send HUP signal to reload consul-template config'
-sudo ps -efww | grep consul-template | awk '{print $2}' | xargs kill -HUP
+kill -HUP $(cat /tmp/ctmpl.pid)
