2bfee629600972d0be114b3cc9888678835539a3
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2017-02-10 15:45:43 -0500

    remove beforeinstall

--- build-scripts/codeDeployInstall.sh
+++ build-scripts/codeDeployInstall.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+
+sudo mv -f  /home/ec2-user/codeDeploy/haproxy-config/ /home/ec2-user/haproxy-config
+
 echo 'send HUP signal to reload consul-template config'
 sudo ps -efww | grep consul-template | awk '{print $2}' | xargs kill -HUP
